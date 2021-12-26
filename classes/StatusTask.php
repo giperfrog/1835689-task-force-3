91d6056fb3931f005813d68473bca9a737b9c29a
Ольга Кашина <giperfrog@gmail.com>
2021-12-26 17:48:25 +0300

    Новый вариант

--- classes/StatusTask.php
+++ classes/StatusTask.php
@@ -9,13 +9,16 @@ class StatusTask
     const STATUS_FAIL = 'failed';
     const ACTION_CANCEL = 'action cancel';
     const ACTION_RESPOND = 'action respond';
+    const ACTION_ACCEPT = 'action accept';
     const ACTION_DONE = 'action done';
     const ACTION_FAIL = 'action fail';
 
     private $customerId;
     private $contractorId;
+    public $current_state;
 
-    private static $statusName = [
+    private static $statusName =
+    [
         self::STATUS_NEW => 'Новое',
         self::STATUS_CANCEL => 'Отменено',
         self::STATUS_IN_WORK => 'В работе',
@@ -23,58 +26,51 @@ class StatusTask
         self::STATUS_FAIL => 'Провалено'
     ];
 
-    private static $actionName = [
+    private static $actionName =
+    [
         self::ACTION_CANCEL => 'Отменить задание',
-        self::ACTION_RESPOND =>'Откликнуться на задание',
+        self::ACTION_RESPOND => 'Откликнуться на задание',
+        self::ACTION_ACCEPT => 'Принять',
         self::ACTION_DONE => 'Выполнить задание',
         self::ACTION_FAIL => 'Отказаться от задания'
     ];
 
-        public function __construct(int $customerId, int $contractorId)
+    public function __construct(int $customerId, int $contractorId, $current_state)
     {
         $this->customerId = $customerId;
         $this->contractorId = $contractorId;
+        $this->current_state = $current_state;
     }
 
-    public static function getNextStatus($action, $current_state)
+    public static function getNextStatus($current_state, $user_id, $action)
     {
-        $current_state = self::$statusName['STATUS_NEW'];
-
-        switch ($current_state) {
-            case 'Новое':
-                if ($action === self::ACTION_CANCEL && self::$customerId) {
-                    return $current_state = self::$statusName['STATUS_CANCEL'];
-                }
-                if ($action === self::ACTION_RESPOND && self::$contractorId) {
-                    return $current_state = self::$statusName['STATUS_IN_WORK'];
-                }
-            case 'В работе':
-                if ($action === self::ACTION_DONE && self::$customerId) {
-                    return $current_state = self::$statusName['STATUS_DONE'];
-                }
-                if ($action === self::ACTION_FAIL && self::$contractorId) {
-                    return $current_state = self::$statusName['STATUS_FAIL'];
-                }
+        if ($current_state === self::$statusName['STATUS_NEW'] && $user_id === self::$customerId && $action === self::ACTION_CANCEL) {
+            return self::$statusName['STATUS_CANCEL'];
+        }
+        if ($current_state === self::$statusName['STATUS_NEW'] && $user_id === self::$customerId && $action === self::ACTION_ACCEPT) {
+            return self::$statusName['STATUS_IN_WORK'];
+        }
+        if ($current_state = self::$statusName['STATUS_IN_WORK'] && $user_id === self::$customerId && $action === self::ACTION_DONE) {
+            return self::$statusName['STATUS_DONE'];
+        }
+        if ($current_state = self::$statusName['STATUS_IN_WORK'] && $user_id === self::$contractorId && $action === self::ACTION_FAIL) {
+            return self::$statusName['STATUS_FAIL'];
         }
     }
 
-    public static function getAction($current_state)
+    public static function getAction($current_state, $user_id)
     {
-        switch ($current_state) {
-            case 'Новое':
-                if (self::$customerId) {
-                    return self::$actionName['ACTION_CANCEL'];
-                }
-                if (self::$contractorId) {
-                    return  self::$actionName['ACTION_RESPOND'];
-                }
-            case 'В работе':
-                if (self::$customerId) {
-                    return self::$actionName['ACTION_DONE'];
-                }
-                if (self::$contractorId) {
-                    return self::$actionName['ACTION_FAIL'];
-                }
+        if ($current_state === self::$statusName['STATUS_NEW'] && $user_id === self::$customerId) {
+            return self::$actionName['ACTION_CANCEL'] && self::$actionName['ACTION_ACCEPT'];
+        }
+        if ($current_state === self::$statusName['STATUS_NEW'] && $user_id === self::$contractorId) {
+            return  self::$actionName['ACTION_RESPOND'];
+        }
+        if ($current_state = self::$statusName['STATUS_IN_WORK'] && $user_id === self::$customerId) {
+            return self::$actionName['ACTION_DONE'];
+        }
+        if ($current_state = self::$statusName['STATUS_IN_WORK'] && $user_id === self::$contractorId) {
+            return self::$actionName['ACTION_FAIL'];
         }
     }
 }
--- test.php
+++ test.php
@@ -2,11 +2,11 @@
 
 require_once('classes/StatusTask.php');
 
-$statusTask = new StatusTask(1, 2);
+$statusTask = new StatusTask(1, 2, 'new');
 
-assert($statusTask->getNextStatus(self::ACTION_CANCEL, 'Новое') === self::$statusName['STATUS_CANCEL']);
-assert($statusTask->getNextStatus(self::ACTION_RESPOND, 'Новое') === self::$statusName['STATUS_IN_WORK']);
-assert($statusTask->getNextStatus(self::ACTION_DONE, 'В работе') === self::$statusName['STATUS_DONE']);
-assert($statusTask->getNextStatus(self::ACTION_FAIL, 'В работе') === self::$statusName['STATUS_FAIL']);
+assert($statusTask->getNextStatus(self::$statusName['STATUS_NEW'], self::$customerId, self::ACTION_CANCEL) === self::$statusName['STATUS_CANCEL']);
+assert($statusTask->getNextStatus(self::$statusName['STATUS_NEW'], self::$customerId, self::ACTION_ACCEPT) === self::$statusName['STATUS_IN_WORK']);
+assert($statusTask->getNextStatus(self::$statusName['STATUS_IN_WORK'], self::$customerId, self::ACTION_DONE) === self::$statusName['STATUS_DONE']);
+assert($statusTask->getNextStatus(self::$statusName['STATUS_IN_WORK'], self::$contractorId, self::ACTION_FAIL) === self::$statusName['STATUS_FAIL']);
 
 echo 'Completed';
